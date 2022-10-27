3c8bdbc7275542121a71fa083800dc6083b8469e
ellrub <ellefsen88@gmail.com>
2022-10-27 12:34:34 +0200

    feature/general
    added larger text on larger devices
    changed positioning on larger devices

--- construction-template/src/container/About/About.jsx
+++ construction-template/src/container/About/About.jsx
@@ -5,22 +5,24 @@ import "./About.scss";
 
 function About() { 
     return (
-        <section className="about__section" >
-            <motion.h2
-                whileInView={{opacity: [0, 1]}}
-                transition={{duration: 1.5, ease: "easeOut"}}
-            >Om Oss</motion.h2>
-            <motion.p
-                whileInView={{opacity: [0, 1]}}
-                transition={{duration: 1.5, ease: "easeOut"}}
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-            ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-            officia deserunt mollit anim id est laborum. 
+        <section className="about__section">
+            <div className="about__container">
+                <motion.h2
+                    whileInView={{opacity: [0, 1]}}
+                    transition={{duration: 1.5, ease: "easeOut"}}
+                >Om Oss</motion.h2>
+                <motion.p
+                    whileInView={{opacity: [0, 1]}}
+                    transition={{duration: 1.5, ease: "easeOut"}}
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum. 
                 </motion.p>
+            </div>
             <div id="tjenester"></div>
         </section>
     )
--- construction-template/src/container/Header/Header.jsx
+++ construction-template/src/container/Header/Header.jsx
@@ -6,30 +6,36 @@ import "./Header.scss"
 function Header() {
     return (
         <header id="hjem">
-            <motion.div
-                whileInView={{opacity: [0, 1]}}
-                transition={{duration: 1.5, ease: "easeOut"}}
-                >
-                <h3>17 års erfaring</h3>
-                <h2>Graving, sprenging eller transport? vi tar jobben</h2>
-                <p>
-                    En erfaren entreprenør som utfører tjenester i Bergen og omeng. Kontakt oss i dag for et uforpliktende tilbud.
-                </p>
-            </motion.div>
-            <motion.div 
-                className="header__buttons"
-                whileInView={{opacity: [0, 1]}}
-                transition={{duration: 1.5, ease: "easeOut"}}>
-                <a className="about" href="#om oss">Om Oss</a>
-                <a className="service" href="#tjenester">Tjenester</a>
-            </motion.div>
-            <motion.div
-            className="header__buttons"
-            whileInView={{opacity: [0, 1]}}
-            transition={{duration: 1.5, ease: "easeOut"}}
-            >
-                <a className="contact" href="#kontakt oss">Kontakt Oss</a>
-            </motion.div>
+            <div className="header__container">
+                <motion.div
+                    className="header__section"
+                    whileInView={{opacity: [0, 1]}}
+                    transition={{duration: 1.5, ease: "easeOut"}}
+                    >
+                    <h3>17 års erfaring</h3>
+                    <h2>Graving, sprenging eller transport? vi tar jobben</h2>
+                    <p>
+                        En erfaren entreprenør som utfører tjenester i Bergen og omeng. 
+                        Kontakt oss i dag for et uforpliktende tilbud.
+                    </p>
+                </motion.div>
+                <div className="header__button-container">
+                    <motion.div 
+                        className="header__buttons"
+                        whileInView={{opacity: [0, 1]}}
+                        transition={{duration: 1.5, ease: "easeOut"}}>
+                        <a className="about" href="#om oss">Om Oss</a>
+                        <a className="service" href="#tjenester">Tjenester</a>
+                    </motion.div>
+                    <motion.div
+                    className="header__buttons"
+                    whileInView={{opacity: [0, 1]}}
+                    transition={{duration: 1.5, ease: "easeOut"}}
+                    >
+                        <a className="contact" href="#kontakt oss">Kontakt Oss</a>
+                    </motion.div>
+                </div>
+            </div>
             <div id="om oss"></div>
         </header>
     )
