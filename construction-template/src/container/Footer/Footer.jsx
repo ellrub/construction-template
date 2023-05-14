f01a4ec6e4e9d12411456886b8e16df7d6c269c8
ellrub <ellefsen88@gmail.com>
2023-05-14 16:05:57 +0200

    Fix
    Removed some unnecessary P's

--- construction-template/src/container/Footer/Footer.jsx
+++ construction-template/src/container/Footer/Footer.jsx
@@ -11,18 +11,16 @@ function Footer() {
             <div className="footer__contacts--container">
                 <div className="footer__contacts--card">
                     <p>Vidar Tveit ( Daglig leder )</p>
-                    <p><a href="tel: +47 464 42 491"><BsFillTelephoneFill /> +47 464 42 491</a></p>
-                    <p><a href="mailto:vidar@boent.no"><BsFillEnvelopeOpenFill /> vidar@boent.no</a></p>
+                    <a href="tel: +47 464 42 491"><BsFillTelephoneFill /> +47 464 42 491</a><br />
+                    <a href="mailto:vidar@boent.no"><BsFillEnvelopeOpenFill /> vidar@boent.no</a>
                 </div>
                 <div className="footer__contacts--card">
                     <p>Øystein Tveit</p>
-                    <p><a href="tel: +47 464 42 492"><BsFillTelephoneFill /> +47 464 42 492</a></p>
-                    <p><a href="mailto:oystein@boent.no"><BsFillEnvelopeOpenFill /> oystein@boent.no</a></p>
+                    <a href="tel: +47 464 42 492"><BsFillTelephoneFill /> +47 464 42 492</a><br />
+                    <a href="mailto:oystein@boent.no"><BsFillEnvelopeOpenFill /> oystein@boent.no</a>
                 </div>
             </div>
-            <p>
-                <a href="mailto:boent@boent.no"><BsFillEnvelopeOpenFill /> boent@boent.no</a>
-            </p>
+                <a className="contacts--card-company" href="mailto:boent@boent.no"><BsFillEnvelopeOpenFill /> boent@boent.no</a>
         </footer>
     )
 }
